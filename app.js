c9105164fad5f4135192dbe3aeb89f9af67cc632
RavinduThaveesha <thaveesha.ravindu2@gmail.com>
2020-02-20 22:17:07 +0800

    completem first version

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const multer = require('multer');
+const { TesseractWorker } = require('tesseract.js');
+const worker = new TesseractWorker();
+
+//storage
+const stroage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads');
+    }, 
+    filename : (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+//uploads
+const upload = multer({storage: stroage}).single('avatar');
+
+//view engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+            if(err) return console.log('error', err);
+
+            worker
+            .recognize(data, 'eng', {tessjs_create_pdf: '1'})
+            .progress(progress => console.log(progress))
+            .then(result => {
+                //res.send(result.text)
+                res.redirect('/download');
+            })
+            .finally(() => {
+                worker.terminate();
+                console.log('process finish.')
+            })
+        })
+    })
+});
+
+app.get('/download', (req, res) => {
+    const file = `${__dirname}/tesseract.js-ocr-result.pdf`;
+    res.download(file);
+});
+
+//start server
+const PORT = 5000 || process.env.PORT
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
